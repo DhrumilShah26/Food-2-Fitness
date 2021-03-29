dee3511999d5fd5c85d39dcfadae083083210f5c
Dhrumil <sdhrumil2605@gmail.com>
2021-03-28 20:32:17 -0400

    final commit

---
+++ project1/build/precache-manifest.1ac84d5806062d4a089ef71fbb3431e0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7167a11c374b8c2c0044c10094165a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06ce2349f132c09c3b43",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "55ea54b75472564ead1b",
+    "url": "/static/css/main.ae92b76c.chunk.css"
+  },
+  {
+    "revision": "06ce2349f132c09c3b43",
+    "url": "/static/js/2.dec055fd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.dec055fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55ea54b75472564ead1b",
+    "url": "/static/js/main.b96b95b1.chunk.js"
+  },
+  {
+    "revision": "8c19395358fbe3240053",
+    "url": "/static/js/runtime-main.d1e082bc.js"
+  },
+  {
+    "revision": "d44ad7f449f2b3ffa2826c5c167d21d4",
+    "url": "/static/media/doc.d44ad7f4.jpg"
+  },
+  {
+    "revision": "fddbf6f0c6d82bc053e08f536686db90",
+    "url": "/static/media/run.fddbf6f0.jpeg"
+  }
+]);
